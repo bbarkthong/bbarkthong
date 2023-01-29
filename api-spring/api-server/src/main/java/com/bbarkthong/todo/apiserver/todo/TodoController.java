3bc1a60451108eeaecf6ae179c70fe2d31473580
bbarkthong <bbarkthong@gmail.com>
2023-01-29 16:34:22 +0900

    spring-web 추가

--- api-spring/api-server/src/main/java/com/bbarkthong/todo/apiserver/todo/TodoController.java
+++ api-spring/api-server/src/main/java/com/bbarkthong/todo/apiserver/todo/TodoController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@RequiredArgsConstructor
-@RestController(value = "/todo")
+// @RequiredArgsConstructor
+@RestController
 @Slf4j
 public class TodoController {
 
-    private final TodoService todoService;
+    // private final TodoService todoService;
 
     @GetMapping("/list")
     public ResponseEntity<?> retrieveTodoList() {
         log.debug("retrieveTodoList call");
         String userId = "임시";
-        List<TodoEntity> todoEntities = todoService.retrieve(userId);
-        todoEntities.stream().forEach(s -> log.debug(s.toString()));
+        // List<TodoEntity> todoEntities = todoService.retrieve(userId);
+        // todoEntities.stream().forEach(s -> log.debug(s.toString()));
         // List<TodoDTO> dtos =
         // todoEntities.stream().map(TodoDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok("aaa");
